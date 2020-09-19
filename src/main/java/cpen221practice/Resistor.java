5f6bd23a559927ae1140799c7309905753f4058b
Ivelina Daiss <ivelina.daiss.ubc@gmail.com>
2020-09-18 17:11:31 -0700

    finished

--- src/main/java/cpen221practice/Resistor.java
+++ src/main/java/cpen221practice/Resistor.java
@@ -1,11 +1,49 @@
 package cpen221practice;
 
 class Resistor {
+    //
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        int colorNumber = 0;
+        switch(color){
+            case "black":
+              colorNumber = 0 ;
+              break;
+            case "brown":
+                colorNumber = 1 ;
+                break;
+            case "red":
+                colorNumber = 2 ;
+                break;
+            case "orange":
+                colorNumber = 3 ;
+                break;
+            case "yellow":
+                colorNumber = 4 ;
+                break;
+            case "green":
+                colorNumber = 5;
+                break;
+            case "blue":
+                colorNumber = 6;
+                break;
+            case "violet":
+                colorNumber = 7;
+                break;
+            case "grey":
+                colorNumber = 8;
+                break;
+            case "white":
+                colorNumber = 9;
+                break;
+            default:
+                break;
+        }
 
+        return colorNumber;
+    }
+//
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String [] resistorColors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        return resistorColors;
     }
 }
